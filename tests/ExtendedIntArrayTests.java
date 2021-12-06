cfbd89fa1412343090fa59324db851a9909f4a32
carte <carte@10.36.91.21>
2021-12-06 13:42:28 -0500

    Test works locally

--- tests/ExtendedIntArrayTests.java
+++ tests/ExtendedIntArrayTests.java
@@ -3,6 +3,7 @@ import org.junit.Test; // Used for writing of Junit tests
 
 
 public class ExtendedIntArrayTests {
+	
 	@Test
 	public void testTesting() {
 		/* This test is just to ensure that all tests can function
