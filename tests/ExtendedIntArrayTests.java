a947426d1f2ec93c20de236b7ba1194352d2be94
carte <carte@10.36.91.21>
2021-12-06 13:37:06 -0500

    Adding buildfile and test file

---
+++ tests/ExtendedIntArrayTests.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.*; 
+import org.junit.Test; // Used for writing of Junit tests
+
+
+public class ExtendedIntArrayTests {
+	@Test
+	public void testTesting() {
+		/* This test is just to ensure that all tests can function
+		 * will be later deleted
+		 */
+		assertEquals(0, 0);
+	}
+}
