92eae44ab968d295967e5042e1a1f846e57e8ee5
carte <carte@10.0.0.151>
2021-12-14 12:37:48 -0500

    More upgrades

--- code/ExtendedIntArray.java
+++ code/ExtendedIntArray.java
@@ -1,8 +1,24 @@
+/* Carter Ibach (20903582)
+ * Final Assignment Coding Challenge
+ * Started December 6th 2021
+ * Finished 
+ * 
+ */
+
+import java.util.Arrays;
 
 public class ExtendedIntArray{
 	private int[] array;
 	
-	public ExtendedIntArray(int[] array) {
+	private int sum = 0;
+	private boolean sumFound = false;
+	private double mean = 0;
+	private boolean meanFound = false;
+	private double stdDev = 0;
+	private boolean stdDevFound = false;
+	private boolean sorted = false;
+	
+ 	public ExtendedIntArray(int[] array) {
 		this.array = array;
 	}
 	
@@ -10,10 +26,25 @@ public class ExtendedIntArray{
 		this.array = new int[0];
 	}
 	
+	public void addToEnd(int value) {
+		this.array = Arrays.copyOf(this.array, this.array.length + 1);
+		
+		this.array[this.array.length - 1] = value;
+		
+		sumFound = false;
+		meanFound = false;
+		stdDevFound = false;
+		sorted = false;
+	}
+	
 	public String toString() {
+		if (getLength() == 0) {
+			return "[]";
+		}
+		
 		String result = "[";
 		
-		for (int i = 0; i < array.length; i++) {
+		for (int i = 0; i < getLength(); i++) {
 			if (result.equals("[")) {
 				result += array[i] + ",";
 			} else {
@@ -21,8 +52,129 @@ public class ExtendedIntArray{
 			}
 		}
 		
-		result = result.substring(0, -1) + "]";
-		
+		result = result.substring(0, result.length()-1) + "]";
+			
 		return result;
+	}
+	
+	public int[] getArray() {
+		return this.array;
+	}
+	
+	public int getLength() {
+		return getArray().length;
+	}
+	
+	private void calculateSum() {
+		if (this.sumFound) {
+			return;
+		}
+		
+		int sum = 0;
+		
+		for (int value: getArray()) {
+			sum += value;
+		}
+		
+		this.sum = sum;
+		sumFound = true;
+	}
+	
+	public int getSum() {
+		if (!sumFound) {
+			this.calculateSum();
+		}
+		return sum;
+	}
+	
+	private void calculateMean() {
+		if (this.meanFound) {
+			return;
+		}
+		
+		if (getLength() == 0) {
+			throw new IllegalArgumentException("Invalid array of length 0");
+		}
+		
+		this.mean = (double) this.getSum()/(double) getLength();
+		meanFound = true;
+	}
+	
+	public double getMean() {
+		this.calculateMean();
+		
+		return this.mean;
+	}
+	
+	private void calculateStdDev() {
+		if (this.stdDevFound) {
+			return;
+		}
+		
+		if (getLength() == 0) {
+			throw new IllegalArgumentException("Invalid array of length 0");
+		}
+		
+		double mean = this.getMean();
+		double deviation = 0.0;
+		
+		for (int value: getArray()) {
+			deviation += Math.pow(value - mean, 2) / (double) array.length;
+		}
+		
+		this.stdDev = Math.sqrt(deviation);
+		stdDevFound = true;
+	}
+	
+	public double getStandardDeviation() {
+		this.calculateStdDev();
+		
+		return this.stdDev;
+	}
+	
+	public boolean isSorted() {
+		return this.isSorted();
+	} 
+	
+	public void sortArray() {
+		if (this.sorted) {
+			return;
+		}
+		
+		int[] array = this.getArray();
+		
+		for (int i = 1; i < getLength(); i++) {
+			for (int j = i; j > 0; j--) {
+				if (array[j] < array[j-1]) {
+					swap(array, j, j-1);
+				}
+			}
+		}
+		
+		this.sorted = true;
+	}
+	
+	private static void swap(int[] array, int first, int second) {
+		int temp = array[first];
+		array[first] = array[second];
+		array[second] = temp;
+	}
+	
+	public int[] find3Numbers(int sum) {
+		if (getLength() < 3) {
+			throw new IllegalArgumentException("Array length less than 3");
+		}
+		
+		sortArray();
+		
+		int[] indexes = new int[] {-1, -1, -1};
+		
+		return indexes;
+	}
+	
+	public static int[] find3Numbers(int[] array, int sum) {
+		ExtendedIntArray goodArray = new ExtendedIntArray(array);
+		
+		return goodArray.find3Numbers(sum);
 	}
 }
--- tests/ExtendedIntArrayTests.java
+++ tests/ExtendedIntArrayTests.java
@@ -1,14 +1,228 @@
+/* Carter Ibach (20903582)
+ * Final Assignment Coding Challenge
+ * Started December 14th 2021
+ * Finished 
+ * 
+ */
+
 import static org.junit.Assert.*; 
 import org.junit.Test; // Used for writing of Junit tests
 
-
 public class ExtendedIntArrayTests {
 	
+	@SuppressWarnings("unused")
 	@Test
-	public void testTesting() {
-		/* This test is just to ensure that all tests can function
-		 * will be later deleted
+	public void testCreateSimpleArray() {
+		/* 
 		 */
-		assertEquals(0, 0);
+		ExtendedIntArray array = new ExtendedIntArray(new int[] {1, 3, 5, 7});
+	}
+	
+	@SuppressWarnings("unused")
+	@Test
+	public void testCreateBlankArray() {
+		/* 
+		 */
+		ExtendedIntArray array = new ExtendedIntArray(new int[] {});
+	}
+	
+	@SuppressWarnings("unused")
+	@Test
+	public void testCreateNoArray() {
+		/* 
+		 */
+		ExtendedIntArray array = new ExtendedIntArray();
+	}
+	
+	@Test
+	public void testSimpleArrayToString() {
+		/* 
+		 */
+		ExtendedIntArray array = new ExtendedIntArray(new int[] {1, 3, 5, 7});
+		
+		assertEquals("[1, 3, 5, 7]", array.toString());
+	}
+	
+	@Test
+	public void testBlankArrayToString() {
+		/* 
+		 */
+		ExtendedIntArray array = new ExtendedIntArray(new int[] {});
+		
+		assertEquals("[]", array.toString());
+	}
+	
+	@Test
+	public void testNoArrayToString() {
+		/* 
+		 */
+		ExtendedIntArray array = new ExtendedIntArray();
+		
+		assertEquals("[]", array.toString());
+	}
+
+	@Test
+	public void testSimpleArrayLength() {
+		/* 
+		 */
+		ExtendedIntArray array = new ExtendedIntArray(new int[] {1, 3, 5, 7});
+		
+		assertEquals(4, array.getLength());
+	}
+	
+	@Test
+	public void testBlankArrayLength() {
+		/* 
+		 */
+		ExtendedIntArray array = new ExtendedIntArray(new int[] {});
+		
+		assertEquals(0, array.getLength());
+	}
+	
+	@Test
+	public void testNoArrayLength() {
+		/* 
+		 */
+		ExtendedIntArray array = new ExtendedIntArray();
+		
+		assertEquals(0, array.getLength());
+	}
+	
+	@Test
+	public void testAddToEnd() {
+		/* 
+		 */
+		ExtendedIntArray array = new ExtendedIntArray();
+		
+		array.addToEnd(1);
+	}
+	
+	@Test
+	public void testAddToEndRepeated() {
+		/* 
+		 */
+		ExtendedIntArray array = new ExtendedIntArray();
+		
+		array.addToEnd(1);
+		array.addToEnd(2);
+		array.addToEnd(3);
+		array.addToEnd(4);
+		array.addToEnd(5);
+	}
+	
+	@Test
+	public void testAddToEndRepeatedToString() {
+		/* 
+		 */
+		ExtendedIntArray array = new ExtendedIntArray();
+		
+		array.addToEnd(1);
+		array.addToEnd(2);
+		array.addToEnd(3);
+		array.addToEnd(4);
+		array.addToEnd(5);
+		
+		assertEquals("[1, 2, 3, 4, 5]", array.toString());
+	}
+	
+	@Test
+	public void testAddToEndRepeatedLength() {
+		/* 
+		 */
+		ExtendedIntArray array = new ExtendedIntArray();
+		
+		assertEquals(0, array.getLength());
+		
+		array.addToEnd(1);
+		array.addToEnd(2);
+		array.addToEnd(3);
+		array.addToEnd(4);
+		array.addToEnd(5);
+		
+		assertEquals(5, array.getLength());
+	}
+	
+	@Test
+	public void testGetSumSimple() {
+		/* 
+		 */
+		ExtendedIntArray array = new ExtendedIntArray(new int[] {1, 3, 5, 7});
+		
+		assertEquals(16, array.getSum());
+	}
+	
+	@Test
+	public void testGetSumBlank() {
+		/* 
+		 */
+		ExtendedIntArray array = new ExtendedIntArray();
+		
+		assertEquals(0, array.getSum());
+	}
+	
+	@Test
+	public void testGetSumZero() {
+		/* 
+		 */
+		ExtendedIntArray array = 
+				new ExtendedIntArray(new int[] {5, 8, 4, -17});
+		
+		assertEquals(0, array.getSum());
+	}
+	
+	@Test
+	public void testGetMeanSimple() {
+		/* 
+		 */
+		ExtendedIntArray array = new ExtendedIntArray(new int[] {1, 3, 5, 7});
+		
+		assertEquals(4, array.getMean(), 0.01);
+	}
+	
+	@Test (expected = IllegalArgumentException.class)
+	public void testGetMeanBlank() {
+		/* 
+		 */
+		ExtendedIntArray array = new ExtendedIntArray();
+		
+		assertEquals(0, array.getMean(), 0.01);
+	}
+	
+	@Test
+	public void testGetMeanZero() {
+		/* 
+		 */
+		ExtendedIntArray array = 
+				new ExtendedIntArray(new int[] {5, 8, 4, -17});
+		
+		assertEquals(0, array.getMean(), 0.01);
+	}
+	
+	@Test
+	public void testGetStdDevSimple() {
+		/* 
+		 */
+		ExtendedIntArray array = new ExtendedIntArray(new int[] {1, 3, 5, 7});
+		
+		assertEquals(Math.sqrt(5), array.getStandardDeviation(), 0.01);
+	}
+	
+	@Test (expected = IllegalArgumentException.class)
+	public void testGetStdDevBlank() {
+		/* 
+		 */
+		ExtendedIntArray array = new ExtendedIntArray();
+		
+		assertEquals(0, array.getStandardDeviation(), 0.01);
+	}
+	
+	@Test
+	public void testGetStdDevZero() {
+		/* 
+		 */
+		ExtendedIntArray array = 
+				new ExtendedIntArray(new int[] {8, 8, 8, 8, 8, 8, 8});
+		
+		assertEquals(0, array.getStandardDeviation(), 0.01);
 	}
 }
